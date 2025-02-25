25d8a4d7bb4176b5db67c5063fc4a41f3d59d9f9
dedMamed <117987060+dedMamed@users.noreply.github.com>
2025-02-25 16:09:00 +0300

    Поправил код еще1

--- name.py
+++ name.py
@@ -1,9 +1,6 @@
 def main():
     name = input("Введите ваше имя: ")
-    age = input("Введите ваш возраст: ")
-    
+    age = input("Введите ваш возраст: ")    
     print(f"Привет, {name}! Вам {age} лет.")
-
-
 if __name__ == "__main__":
     main()
