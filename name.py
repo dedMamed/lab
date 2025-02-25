4ad7a4bfbc6607a8d06b9af5047420381d3086c4
dedMamed <117987060+dedMamed@users.noreply.github.com>
2025-02-25 13:31:13 +0300

    Init commit: name

---
+++ name.py
@@ -0,0 +1,10 @@
+def main():
+    name = input("Введите ваше имя: ")
+    
+    age = input("Введите ваш возраст: ")
+    
+    print(f"Привет, {name}! Вам {age} лет.")
+
+if __name__ == "__main__":
+    main()
+
